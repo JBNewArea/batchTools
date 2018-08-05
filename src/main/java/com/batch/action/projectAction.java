43d7eedd2dd2cfe5cd8e26a0c0af59edc31256ae
guanzheyuan <834155417@qq.com>
2018-08-05 14:41:30 +0800

    更新

--- src/main/java/com/batch/action/projectAction.java
+++ src/main/java/com/batch/action/projectAction.java
@@ -15,28 +15,45 @@ public class projectAction {
 	
 	public static void execute(){
 		Connection conn = JDBC.connect();
-		String query_project="select build_detail_place from project_declare";
-		PreparedStatement pstmt;
+		String query_project="select id,build_detail_place from project_declare";
+		PreparedStatement pstmt = null;
 		try {
 			pstmt = (PreparedStatement)conn.prepareStatement(query_project);
 			ResultSet rs = pstmt.executeQuery();
 			int nullCount=0;
 			while (rs.next()) {
-	            String resLong = mapLongItudeUtils.getLngAndLat(rs.getString(1));
+				System.out.println(rs.getString(2));
+	            String resLong = mapLongItudeUtils.getLngAndLat(rs.getString(2));
 	            if(!resLong.isEmpty()){
-	            	 System.out.println(resLong);
+	            	//区分经纬度
+	            	 String[] longItArr = resLong.split(",");
+	            	 String update_Project = "update project_declare set longitude='"+longItArr[0]+"' and latitude='"+longItArr[1]+"'  where id='"+rs.getString(1)+"'";
+	            	 boolean upres = JDBC.execute(update_Project,"");
+	            	 System.out.println(upres);
 	            }else{
-	            	 System.out.println(rs.getString(1));
 	            	 nullCount++;
 	            }
 	        }
 			System.out.println(nullCount);
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}finally{
+			try {
+				if(pstmt !=null){
+					pstmt.close();
+				}
+				if(conn !=null){
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
 	public static void main(String[] args) {
-		execute();
+		 String update_Project = "update project_declare set longitude=?    where id=5446456451";
+    	 boolean upres = JDBC.execute(update_Project,"999");
+    	 System.out.println(upres);
 	}
 }
